22d2d0c46b20d3c90e90b311949745ee1f51444c
chenbm4 <chenbm4@gmail.com>
2022-06-24 18:54:56 -0700

    with oculus support (broken)

---
+++ Assets/DropBall.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DropBall : MonoBehaviour
+{
+    public BoxCollider shootingZone;
+    public GameObject basketball;
+    public XRGrabInteractable basketballGrabInteractable;
+    public XRBaseInteractor hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hand.allowSelect = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (basketballGrabInteractable.isSelected && other.gameObject.name == "Main Camera")
+        {
+            Debug.Log($"{OVRInput.GetLocalControllerAcceleration((OVRInput.Controller.RTouch))}");
+            //Debug.Log("Drop Ball");
+            //hand.allowSelect = false;
+        }
+    }
+}
